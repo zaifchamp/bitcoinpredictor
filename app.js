dac4de70ece782cd22a6418c01fb5b4de9230469
Huzaifa Tahir <huzaifa_tah@yahoo.com>
2018-03-24 18:45:28 +0500

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end("Hello, World!\n");
-}).listen(process.env.PORT);
-
-console.log('App is running...');
